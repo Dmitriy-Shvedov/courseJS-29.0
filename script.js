6666be58ead5036a051932de1fb43ddc1b2d4d8f
AmbalbIsh <ambalbIsh@gmail.com>
2023-02-01 09:08:44 +0300

    сделал задание 2

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let title
-let screens
-let screenPrice
-let rollback
-let fullPrice
-let adaptive
+let title = "Какое название проекта?";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 23;
+let rollback = 1;
+let fullPrice = 75;
+let adaptive = true;
 
-alert ("start")
-console.log('start log');
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " +  screenPrice + " рублей/долларов/гривен/юани");
+console.log("Стоимость разработки сайта " +  fullPrice + " рублей/долларов/гривен/юани");
+console.log(screens.toLowerCase());
+console.log(fullPrice * (rollback/100));
+
+
+// Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани
