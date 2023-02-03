ede018ef311173709124c5fb3053d41057b315cf
AmbalbIsh <ambalbIsh@gmail.com>
2023-02-03 23:37:36 +0300

    задание на курсе 3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict'
 let title = "Какое название проекта?";
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 23;
@@ -13,3 +14,35 @@ console.log("Стоимость верстки экранов " +  screenPrice +
 console.log("Стоимость разработки сайта " +  fullPrice + " рублей/долларов/гривен/юани");
 console.log(screens.toLowerCase().split());
 console.log(fullPrice * (rollback/100));
+
+title = prompt('Как называется ваш проект?');
+console.log(title);
+screens = prompt('Какие типы экранов нужно разработать?', 'Простые/Сложные/Интерактивные');
+console.log(screens);
+screenPrice = +prompt('Сколько будет стоить данная работа?', '1200');
+console.log(screenPrice);
+adaptive = confirm('Нужен ли адаптив на сайте?')
+console.log(adaptive);
+let addService = prompt('Какой дополнительный тип услуги нужен?');
+console.log(addService);
+let price = +prompt('Сколько это будет стоить?', '1000');
+let addService2 = prompt('Какой дополнительный тип услуги нужен?');
+let price2 = +prompt('Сколько это будет стоить?', '1000');
+fullPrice = screenPrice + price + price2;
+console.log(fullPrice);
+let servicePercentPrice = +fullPrice - 1200;
+console.log(Math.ceil(servicePercentPrice));
+
+if (fullPrice >= 30000){
+  alert('Даем скидку в 10%');
+} 
+if (fullPrice >= 15000){
+  alert('Даем скидку в 5%');
+} 
+if (fullPrice < 15000 && fullPrice >= 0){
+  alert('Скидка не предусмотрена');
+} 
+if (fullPrice == -1){
+  alert('Что то пошло не так');
+}
+
