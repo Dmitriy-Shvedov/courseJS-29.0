ef569a47120e8522e83603b6a733519ef197628f
AmbalbIsh <ambalbIsh@gmail.com>
2023-02-15 16:22:02 +0300

    Задание 5

--- script.js
+++ script.js
@@ -1,60 +1,101 @@
 'use strict'
 
-let title = prompt('Как называется ваш проект?')
-let screens = prompt('Какие типы экранов нужно разработать?', 'Простые/Сложные/Интерактивные')
-let screenPrice = +prompt('Сколько будет стоить данная работа?', '1200')
-let adaptive = confirm('Нужен ли адаптив на сайте?')
-let addService = prompt('Какой дополнительный тип услуги нужен?')
-let addPrice = +prompt('Сколько это будет стоить?', '1000')
-let addService2 = prompt('Какой дополнительный тип услуги нужен?')
-let addPrice2 = +prompt('Сколько это будет стоить?', '1000')
-let rollback = 1
+let title 
+let screens 
+let screenPrice 
+let adaptive 
+let addService 
+let addService2 
+let rollback = 10
+let allServicePrices
+let fullPrice
+let servicePercentPrice
 
-
-const getAllServicePrices = function () {
-  return addPrice + addPrice
+const isNumber = function(num){
+  return !isNaN(parseFloat(num)) && isFinite(num)
 }
 
-const allServicePrices = getAllServicePrices()
+const asking = function () {
+  title = prompt('Как называется ваш проект?', 'калькулятор')
+  screens = prompt('Какие типы экранов нужно разработать?', 'Простые/Сложные/Интерактивные')
 
-function getFullPrice() {
+  // screenPrice = prompt('Сколько будет стоить данная работа?')
+  
+  // while (!isNumber(screenPrice)) {
+  //   screenPrice = prompt('Сколько будет стоить данная работа?')
+    
+  // }
+  do {
+    screenPrice = prompt('Сколько будет стоить данная работа?')
+  } while (!isNumber(screenPrice));
+    
+  
+
+  adaptive = confirm('Нужен ли адаптив на сайте?')
+}
+
+const getAllServicePrices = function () {
+  let sum = 0
+  for (let i = 0; i < 2; i++) {
+
+    if (i === 0) {
+        addService = prompt('Какой дополнительный тип услуги нужен?', 'попап')
+    } else if (i === 1) {
+        addService2 = prompt('Какой дополнительный тип услуги нужен?', 'модалка')
+    }
+
+    sum += +prompt('Сколько это будет стоить?', '1000')
+    
+  }
+  return sum
+  // return addPrice + addPrice2
+}
+
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+}
+
+const getFullPrice = function () {
   return screenPrice + allServicePrices
 }
 
-let fullPrice = getFullPrice()
+const getServicePercentPrice = function () {
+  return fullPrice - (fullPrice * (rollback / 100))
+}
+console.log(getServicePercentPrice);
 
 
 const getTitle = (str) => {
-    return title.trim()[0].toUpperCase() + title.trim().slice(1).toLowerCase()
-     
+  let title = str.trim()
+  let result = title.charAt(0).toUpperCase() + title.slice(1).toLowerCase();
+  return result
 }
-console.log(getTitle);
 
+const getRollbackMessage = function (prise) {
+  if (prise >= 30000) {
+    return 'Даем скидку в 10%'
+  } else if (prise >= 15000 && prise < 30000) {
+    return 'Даем скидку в 5%'
+  } else if (prise < 15000 && prise >= 0) {
+    return 'Скидка не предусмотрена'
+  } else {
+    return 'Что то пошло не так'
+  }
+}
+asking()
+allServicePrices = getAllServicePrices()
+fullPrice = getFullPrice() 
+servicePercentPrice = getServicePercentPrice()
 title = getTitle(title);
-console.log(title)
-function getServicePercentPrices() {
-  return fullPrice - (fullPrice * (rollback / 100))
-}
-
-let servicePercentPrice = getServicePercentPrices()
-
-
-
-// if (fullPrice >= 30000){
-//   alert('Даем скидку в 10%');
-// } else if (fullPrice >= 15000 && fullPrice < 30000){
-//   alert('Даем скидку в 5%');
-// } else if (fullPrice < 15000 && fullPrice >= 0){
-//   alert('Скидка не предусмотрена');
-// } else if (fullPrice < 0){
-//   alert('Что то пошло не так');
-// }
 
 getAllServicePrices('allServicePrices')
-// getRollbackMessage()
-getServicePercentPrices()
 
-// console.log(showTypeOf);
-console.log(screens.length);
+console.log('allServicePrices', allServicePrices);
+
+console.log(showTypeOf);
 console.log(title);
+console.log(screens);
+console.log(fullPrice)
+console.log(servicePercentPrice)
+console.log('Стоимость верстки экранов ' + screenPrice + ' юани и стоимсоть разработки сайта ' + fullPrice + ' юани');
 
