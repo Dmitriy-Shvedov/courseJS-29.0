259667de3b86ff2d60d600303b126a2048d39d08
AmbalbIsh <ambalbIsh@gmail.com>
2023-02-24 20:32:03 +0300

    Задание 10

--- js/script.js
+++ js/script.js
@@ -14,7 +14,35 @@ let fullPrice = document.getElementsByClassName('total-input')[3];
 let rollbackPrice = document.getElementsByClassName('total-input')[4];
 let screen = document.querySelectorAll('.screen');
 
+//book
+let order = document.querySelectorAll('.book')
+let bodyImg = document.querySelector('body')
+let orderH2 = document.querySelectorAll('.book > h2 > a')
+const del = document.querySelector('.adv')
+let orderlist = document.querySelectorAll('.book > ul > li')
+let clonelist = document.querySelectorAll('.chapter')
+console.log(clonelist);
+const clonelistElem = clonelist[0].cloneNode(true)
+console.log(clonelistElem);
 
+
+order[0].before(order[1]);
+order[5].after(order[2]);
+order[3].before(order[4]);
+
+orderH2[4].textContent = 'Книга 3. this и Прототипы Объектов';
+bodyImg = document.body.style.backgroundImage = "url('../book/image/you-dont-know-js.jpg')";
+del.remove();
+
+
+orderlist[4].before(orderlist[6]);
+orderlist[4].before(orderlist[8]);
+orderlist[48].before(orderlist[55]);
+orderlist[51].before(orderlist[48]);
+// clonelist.append(orderlist[56]);
+orderlist[25].append(clonelistElem)
+
+clonelistElem.textContent = 'Глава 8: За пределами ES6'
 
 
 
@@ -135,13 +163,21 @@ let screen = document.querySelectorAll('.screen');
 
 
 console.log(title[0]);
-console.log(btn);
-console.log(plus);
-console.log(percentItem);
-console.log(numberItem);
-console.log(inputRange);
-console.log(span);
-console.log(input);
-console.log(screen);
+// console.log(btn);
+// console.log(plus);
+// console.log(percentItem);
+// console.log(numberItem);
+// console.log(inputRange);
+// console.log(span);
+// console.log(priceLayout);
+// console.log(screen);
+
+console.dir(order);
+console.log(orderH2);
+console.log(del);
+console.log(orderlist);
+console.log(orderlist[6]);
+
+
 
 
